ff7c8e29d459cda1cd54f51aefc2779e6e4e7669
sidharthr2001 <115485847+sidharthr2001@users.noreply.github.com>
2022-10-19 11:06:59 +0530

    Update app.py

--- Assignment/Assignment/Team leader_Sidharth/Assignment2/Assignment-2.2/contact_page(with CSS)/app.py
+++ Assignment/Assignment/Team leader_Sidharth/Assignment2/Assignment-2.2/contact_page(with CSS)/app.py
@@ -1,1 +1,19 @@
+from flask import Flask,render_template,request
+from werkzeug.utils import secure_filename
 
+app=Flask(__name__)
+
+@app.route('/upload')
+def uploadfile():
+    return render_template('main.html')
+
+@app.route('/uploader',methods=['GET','POST'])
+def uploaderfile():
+    if request.method == 'POST':
+        f=request.files['myfile']
+        f.save(secure_filename(f.filename))
+        return 'file uploaded successfully'
+
+
+if __name__=='__main__':
+    app.run(debug=True)
